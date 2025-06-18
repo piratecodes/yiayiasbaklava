76246510a26d6dd9b6d8292d283fb2f57ad83a7b
piratecodes <subhamsarkar1999@gmail.com>
2025-06-18 14:51:49 +0530

    browser update correct

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
 import localfont from "next/font/local"
-import dynamic from 'next/dynamic'
+import Script from 'next/script'
 import { Analytics } from "@vercel/analytics/next"
 import "@/style/globals.css";
 
-const BrowserUpdate = dynamic(()=> import('browser-update'), {ssr: false})
+
 
 
 //file add
@@ -49,7 +49,7 @@ export default function RootLayout({ children }) {
           <Nav />
           {children}
           <Analytics />
-          <BrowserUpdate />
+          <Script id="browser-update" dangerouslySetInnerHTML={{ __html: `var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,api:2025.06 }; function $buo_f(){ var e = document.createElement("script"); e.src = "https://browser-update.org/update.min.js"; document.body.appendChild(e); }; try {document.addEventListener("DOMContentLoaded", $buo_f,false)} catch(e){window.attachEvent("onload", $buo_f)}` }} />
           <Footer />
         </Providers>
       </body>
