d95acd58449777f5ab3802bd9d68628b4175d779
piratecodes <subhamsarkar1999@gmail.com>
2025-07-06 04:58:41 +0530

    keen update

--- src/componant/voting/votingPage.jsx
+++ src/componant/voting/votingPage.jsx
@@ -55,14 +55,14 @@ function votingPage({vote, token}) {
                     {vote.data.map((i, j)=>{
                         return <div key={j} className={`p-3.5 flex flex-col items-center border border-gray-200 rounded-lg shadow-sm md:flex-row ${i.is_voted == 1 ? "bg-sky-500" : "bg-white"}`}>
                             {/* <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt={i.name} /> */}
-                            <figure className='relative w-32 h-28 md:h-48 md:w-48 md:rounded-none md:rounded-s-lg overflow-hidden'><Image fill objectFit='contain' src={i.product_image} alt={i.product_name} draggable="false" /></figure>
+                            <figure className='relative w-32 h-32 md:h-48 md:w-48 md:rounded-none md:rounded-s-lg overflow-hidden'><Image fill objectFit='contain' src={i.product_image} alt={i.product_name} draggable="false" /></figure>
                             <div className="flex flex-col justify-between p-4 leading-normal">
-                                <p className="mb-3 font-semibold text-blue-500">Vote Count: {i.vote_count}</p>
+                                <p className={`mb-3 font-semibold ${i.is_voted == 0 ? "text-sky-500" : "text-white"} `}>Vote Count: {i.vote_count}</p>
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{i.product_name}</h5>
                                 <p className="mb-3 font-normal text-gray-900">{i.description}</p>
                                 <p className="mb-3 font-normal text-gray-900">$ {i.product_price}</p>
                             </div>
-                            <button type="button" onClick={(e)=> handleVote(e, i.product_id)} className="ml-auto px-4 py-2 text-md font-medium text-center inline-flex items-center text-white bg-sky-300 rounded-lg hover:bg-sky-500 cursor-pointer"> <BsFileArrowUpFill /> Vote </button>
+                            <button type="button" onClick={(e)=> handleVote(e, i.product_id)} className="w-full mx-auto md:w-fit md:mx-0 md:ml-auto px-4 py-2 text-md font-medium text-center inline-flex items-center text-white bg-sky-300 rounded-lg hover:bg-sky-500 cursor-pointer"> <BsFileArrowUpFill /> Vote </button>
                         </div>
                     })}
                 </section>
