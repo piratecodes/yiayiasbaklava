04b94c5d37740e6ce5d33b35da7b136256f884a3
piratecodes <subhamsarkar1999@gmail.com>
2025-07-06 05:20:39 +0530

    hom page video mountain divider adjust

--- src/componant/homePage/home.jsx
+++ src/componant/homePage/home.jsx
@@ -68,19 +68,19 @@ export default function Home({home, token}) {
         </svg>
       </header> */}
       <header className="w-full h-full relative -translate-y-20">
-        <video className="hidden md:block z-10 w-full h-auto object-cover brightness-90" autoPlay muted playsInline loop>
+        <video className="hidden lg:block z-10 w-full h-auto object-cover brightness-90" autoPlay muted playsInline loop>
           <source src="/Horizontal-Baklava.webm" type="video/mp4" />
         </video>
-        <video className="md:hidden z-10 w-auto h-screen object-cover brightness-90" autoPlay muted playsInline loop>
+        <video className="lg:hidden z-10 w-auto h-screen object-cover brightness-90" autoPlay muted playsInline loop>
           <source src={home.data.slider[0].image} type="video/mp4" />
         </video>
-        <svg className='absolute -bottom-2 z-20 rotate-180' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+        <svg className='absolute inset-x-0 lg:h-24 w-full -bottom-2 z-20 rotate-180' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
           <path className="fill-white" opacity="0.33" d="M473,67.3c-203.9,88.3-263.1-34-320.3,0C66,119.1,0,59.7,0,59.7V0h1000v59.7 c0,0-62.1,26.1-94.9,29.3c-32.8,3.3-62.8-12.3-75.8-22.1C806,49.6,745.3,8.7,694.9,4.7S492.4,59,473,67.3z"></path>
           <path className="fill-white" opacity="0.66" d="M734,67.3c-45.5,0-77.2-23.2-129.1-39.1c-28.6-8.7-150.3-10.1-254,39.1 s-91.7-34.4-149.2,0C115.7,118.3,0,39.8,0,39.8V0h1000v36.5c0,0-28.2-18.5-92.1-18.5C810.2,18.1,775.7,67.3,734,67.3z"></path>
           <path className="fill-white" d="M766.1,28.9c-200-57.5-266,65.5-395.1,19.5C242,1.8,242,5.4,184.8,20.6C128,35.8,132.3,44.9,89.9,52.5C28.6,63.7,0,0,0,0 h1000c0,0-9.9,40.9-83.6,48.1S829.6,47,766.1,28.9z"></path>
         </svg>
-        <div className='absolute left-10 md:inset-x-10 bottom-1/6 z-30'>
-          <p className='font-semibold font-mono font-lg lg:text-2xl text-center text-white'>Retail pieces are larger than shown</p>
+        <div className='absolute left-5 md:left-10 md:inset-x-10 bottom-1/6 z-30'>
+          <p className='font-semibold font-mono text-md md:text-xl lg:text-2xl text-center text-white'>Retail pieces are larger than shown</p>
           <Link href="/order" className={`mt-1.5 flex items-center w-fit md:mx-auto px-4 py-2 text-md font-medium hover:text-gray-60 rounded-4xl bg-black text-white`}>
               <svg className="w-4 h-4 mr-2 fill-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 544 512">
                 <path d="M527.79 288H290.5l158.03 158.03c6.04 6.04 15.98 6.53 22.19.68 38.7-36.46 65.32-85.61 73.13-140.86 1.34-9.46-6.51-17.85-16.06-17.85zm-15.83-64.8C503.72 103.74 408.26 8.28 288.8.04 279.68-.59 272 7.1 272 16.24V240h223.77c9.14 0 16.82-7.68 16.19-16.8zM224 288V50.71c0-9.55-8.39-17.4-17.84-16.06C86.99 51.49-4.1 155.6.14 280.37 4.5 408.51 114.83 513.59 243.03 511.98c50.4-.63 96.97-16.87 135.26-44.03 7.9-5.6 8.42-17.23 1.57-24.08L224 288z" />
